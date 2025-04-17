10beb8a828e5f8ce8cc614a4a89a866981a59a9a
jihad wael <144389076+JihadWael099@users.noreply.github.com>
2025-04-17 05:11:45 +0200

    orgin done

--- src/main/java/org/fawry/reportapi/controller/ReportController.java
+++ src/main/java/org/fawry/reportapi/controller/ReportController.java
@@ -6,15 +6,13 @@ import org.fawry.reportapi.service.CycleReportService;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/reports")
+@CrossOrigin(origins = "*")
 public class ReportController {
 
     private final EvaluationService evaluationService;
